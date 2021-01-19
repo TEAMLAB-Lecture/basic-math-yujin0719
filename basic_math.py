8837f338a388621417637a6b14aab32c79e289d8
yujin <yujin1760@naver.com>
2021-01-19 17:14:52 +0900

    Update my code

--- basic_math.py
+++ basic_math.py
@@ -24,7 +24,7 @@ def get_greatest(number_list):
             >>> bm.get_greatest(number_list)
             99
     """
-    greatest_number = None
+    greatest_number = sorted(number_list)[-1]
     return greatest_number
 
 
@@ -45,7 +45,7 @@ def get_smallest(number_list):
             >>> bm.get_smallest(number_list)
             11
     """
-    smallest_number = None
+    smallest_number = sorted(number_list)[0]
     return smallest_number
 
 
@@ -66,7 +66,7 @@ def get_mean(number_list):
             >>> bm.get_mean(number_list)
             47
     """
-    mean = None
+    mean = sum(number_list)/len(number_list);
     return mean
 
 
@@ -90,5 +90,5 @@ def get_median(number_list):
             >>> bm.get_median(number_list2)
             35.5
     """
-    median = None
+    median = sorted(number_list)[len(number_list)//2] if len(number_list) % 2 != 0 else (sorted(number_list)[len(number_list) //2 -1] + sorted(number_list)[len(number_list) //2]) / 2
     return median
